e2158f3272861592131580171cc7519f5aacdaa4
fooltomb <fool_tomb@sina.com>
2018-12-19 17:20:43 +0800

    1219

--- SWdaily/dailyreport/views/user_view.py
+++ SWdaily/dailyreport/views/user_view.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from dailyreport.models import Users,Project,UserToProject,Report
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 
 def searchUser(request):
     if not request.session['is_login']:
@@ -75,6 +76,20 @@ def addUser(request):
     if request.method=='GET':
         return HttpResponseRedirect(reverse('dailyreport:users'))
     if request.method=='POST':
-        return HttpResponse("ha")
+        op=request.POST['option']
+        if op=="addnew":
+            obj,created=Users.objects.get_or_create(name=request.POST['newuser'])
+            str="已增加新员工"+request.POST['newuser'] if created else "已有同名员工存在，请换个姓名"
+            return HttpResponse(str)
+        a=Users.objects.get(id=request.POST['user'])
+        if op=="reset":
+            a.password=make_password('123456')
+            a.save()
+            return HttpResponse("重制"+a.name+"的密码，新密码:123456")
+        elif op=="unactive":
+            a.active=False
+            a.save()
+            return HttpResponse("已将员工"+a.name+"设置为离职")
+
 
 
